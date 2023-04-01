696de5ca18390304a8db65f1d0eea00d59be261b
divadde <davidedistef@yahoo.com>
2023-04-01 15:54:27 +0200

    osservazione sull'intero sistema

--- src/CSM/MainProva.java
+++ src/CSM/MainProva.java
@@ -9,32 +9,21 @@ import java.util.Random;
 public class MainProva {
 
     public static void main(String[] args){
+
         Random r = new Random();
 
-        double mu0 = 0.2;
-        double mu1 = 0.5;
-        double mu2 = 0.8;
-        double[] mu3 = {0.4,0.1};
-        double[] a = {0.95,0.05};
-        int n4 = 2;
-        double mu4 = 0.9;
-        Double[] q = {0.2,0.3,0.3,0.2};
-        Double[] delay = {0.5,1.5};
-
-        int numClients = 20;
-        int numServersS1 = 3; //pare che il grado di parallelismo consigliato sia 3.
-
-        double tEnd = 36000;
-
-        Distribution ds0 = new ExponentialDistribution(r,mu0);
-        Distribution ds1 = new ExponentialDistribution(r,mu1);
-        Distribution ds2 = new ExponentialDistribution(r,mu2);
-        Distribution ds3 = new HyperExponentialDistribution(r,a,mu3);
-        Distribution ds4 = new ErlangDistribution(r,n4,mu4);
+        //Creazione distribuzioni di probabilità
+        Distribution ds0 = new ExponentialDistribution(r,Parameters.mu0);
+        Distribution ds1 = new ExponentialDistribution(r,Parameters.mu1);
+        Distribution ds2 = new ExponentialDistribution(r,Parameters.mu2);
+        Distribution ds3 = new HyperExponentialDistribution(r,Parameters.a,Parameters.mu3);
+        Distribution ds4 = new ErlangDistribution(r,Parameters.n4,Parameters.mu4);
         Distribution dr = new UniformDistribution(r,0,1);
 
-        ControlMachine cm = new Simulation(tEnd);
+        //Creazione ControlMachine
+        ControlMachine cm = new Simulation(Parameters.tEnd);
 
+        //Creazione stazioni
         AbstractStation s0 = new ReflectiveStation();
         AbstractStation s1 = new Station();
         AbstractStation s2 = new Station();
@@ -42,53 +31,55 @@ public class MainProva {
         AbstractStation s4 = new Station();
         AbstractStation router = new Router();
 
-        Observer os0 = new Observer();
-        Observer os1 = new Observer(); Observer os2 = new Observer(); Observer os3 = new Observer(); Observer os4 = new Observer();
+        //Creazione observers
+        Observer os0 = new Observer(),os1 = new Observer(),os2 = new Observer(),os3 = new Observer(),os4 = new Observer();
 
-        AbstractStation[] acqS0 = {s1};
-        s0.send("init",ds0,acqS0,numClients,os0,tEnd);
+        //Inizializzazione delle stazioni
+        AbstractStation[] acqS0 = {s1}; //Acquaintances
+        s0.send("init",ds0,acqS0,Parameters.numClients,os0,Parameters.tEnd);
 
-        Path path = new Path();
-        AbstractStation[] acqS1 = {router};
-        s1.send("init",ds1,acqS1,numServersS1,1,os1,tEnd);
+        Path path = new Path(); //Creazione del Path per il calcolo del grado di parallelismo di S1
+        AbstractStation[] acqS1 = {router}; //Acquaintances
+        s1.send("init",ds1,acqS1,Parameters.numServersS1,1,os1,Parameters.tEnd);
         s1.send("setPath",path);
 
-        AbstractStation[] acqR = {s0,s2,s3,s4};
-        router.send("init",dr,acqR,q,delay);
+        AbstractStation[] acqR = {s0,s2,s3,s4}; //Acquaintances
+        router.send("init",dr,acqR,Parameters.q,Parameters.delay);
 
-        AbstractStation[] acqS234 = {s1};
-        s2.send("init",ds2,acqS234,1,2,os2,tEnd);
-        s3.send("init",ds3,acqS234,1,3,os3,tEnd);
-        s4.send("init",ds4,acqS234,1,4,os4,tEnd);
+        AbstractStation[] acqS234 = {s1}; //Acquaintances
+        s2.send("init",ds2,acqS234,1,2,os2,Parameters.tEnd);
+        s3.send("init",ds3,acqS234,1,3,os3,Parameters.tEnd);
+        s4.send("init",ds4,acqS234,1,4,os4,Parameters.tEnd);
 
-
+        //Inizio simulazione
         cm.controller();
 
+        //Risultati della simulazione
         System.out.println("\n Resume stazione 1:");
         System.out.println("Sojourn time: "+os1.sojournTime());
-        System.out.println("Throughput: "+os1.throughput(tEnd));
-        System.out.println("Utilization: "+os1.utilization(tEnd));
-        System.out.println("Mean number of clients in service: "+path.mean(tEnd));
+        System.out.println("Throughput: "+os1.throughput(Parameters.tEnd));
+        System.out.println("Utilization: "+os1.utilization(Parameters.tEnd));
+        System.out.println("Mean number of clients in service: "+path.mean(Parameters.tEnd));
 
         System.out.println("\n Resume stazione 2:");
         System.out.println("Sojourn time: "+os2.sojournTime());
-        System.out.println("Throughput: "+os2.throughput(tEnd));
-        System.out.println("Utilization: "+os2.utilization(tEnd));
+        System.out.println("Throughput: "+os2.throughput(Parameters.tEnd));
+        System.out.println("Utilization: "+os2.utilization(Parameters.tEnd));
 
         System.out.println("\n Resume stazione 3:");
         System.out.println("Sojourn time: "+os3.sojournTime());
-        System.out.println("Throughput: "+os3.throughput(tEnd));
-        System.out.println("Utilization: "+os3.utilization(tEnd));
+        System.out.println("Throughput: "+os3.throughput(Parameters.tEnd));
+        System.out.println("Utilization: "+os3.utilization(Parameters.tEnd));
 
         System.out.println("\n Resume stazione 4:");
         System.out.println("Sojourn time: "+os4.sojournTime());
-        System.out.println("Throughput: "+os4.throughput(tEnd));
-        System.out.println("Utilization: "+os4.utilization(tEnd));
+        System.out.println("Throughput: "+os4.throughput(Parameters.tEnd));
+        System.out.println("Utilization: "+os4.utilization(Parameters.tEnd));
 
         System.out.println("\n Resume intero sistema:");
         System.out.println("Sojourn time: "+os0.sojournTime());
-        System.out.println("Throughput: "+os0.throughput(tEnd));
-        System.out.println("Utilization: "+os0.utilization(tEnd));
+        System.out.println("Throughput: "+os0.throughput(Parameters.tEnd));
+        System.out.println("Utilization: "+os0.utilization(Parameters.tEnd));
     }
 
 }
---
+++ src/CSM/Parameters.java
@@ -0,0 +1,16 @@
+package CSM;
+
+public final class Parameters {
+
+    final static double mu0 = 0.2, mu1 = 0.5, mu2 = 0.8, mu4 = 0.9;
+    final static double[] mu3 = {0.4,0.1};
+    final static double[] a = {0.95,0.05};
+    final static int n4 = 2;
+    final static Double[] q = {0.2,0.3,0.3,0.2};
+    final static Double[] delay = {0.5,1.5};
+
+    final static int numClients = 20;
+    final static int numServersS1 = 3; //Pare che il grado di parallelismo consigliato sia 3.
+
+    final static double tEnd = 36000;
+}
